f45612ef1d1cbeca514a875c68cc19b60bf06a41
Felix Blanco <fxb6476@g.rit.edu>
2019-10-27 15:42:01 -0400

    Changed up example so that it learns to classify based on unity line. Only positive values.

--- main.c
+++ main.c
@@ -8,12 +8,23 @@
 int main(){
 
     // TESTING FC_ANN
+    // This will try to teach the network to identify two clases.
+    // Class 1 = everything above the identity line y = x.
+    // Class 0 = everything bellow the identity line.
     FC_ANN ann1 = {3, {2, 3, 1}};
-    float in_data[][2] = {{1.0, 1.0},
-                          {1.0, 0.0},
-                          {0.0, 1.0},
-                          {0.0, 0.0}};
+    float in_data[][2] = {{1.0, 2.0},
+                          {6.0, 10.0},
+                          {5.0, 8.0},
+                          {3.0, 40.0},
+                          {23.0, 6.0},
+                          {4.0, 1.0},
+                          {10.0, 8.0},
+                          {2.0, 1.0}};
     float out_data[][1] = {{0.0},
+                           {0.0},
+                           {0.0},
+                           {0.0},
+                           {1.0},
                            {1.0},
                            {1.0},
                            {1.0}};
@@ -27,7 +38,7 @@ int main(){
 
         printf("------------------------- Round #%d -------------------------\n", rounds);
 
-        for(int i = 0; i < 4; i++){
+        for(int i = 0; i < 8; i++){
 
             printf("[+] -------------------- Sample #%d output...\n", i+1);
 
@@ -49,6 +60,19 @@ int main(){
     }
 
     printANN(&ann1);
+
+    float new_data[] = {1.0, 100.0};
+    printf("Testing on data it has never seen lol %.3f, %.3f...\n", new_data[0], new_data[1]);
+
+    outputs = feedANN(&ann1, new_data, 2);
+
+    printf("Predicted class should be 0.0\n");
+    printf("Networks prediction...\n");
+    printMatrix(&outputs[ann1.n_layers-1]);
+
+    for(int i=0; i < ann1.n_layers; i++){
+        delMatrix(&outputs[i]);
+    }
 
     free(outputs);
 
